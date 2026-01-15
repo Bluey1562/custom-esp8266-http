62deb82c4b4b52fec7d549008d9a5e732b5d014c
Bluey1562 <rizalakmal@idn.sch.id>
2026-01-15 14:24:19 +0700

    Create main.ts

---
+++ main.ts
@@ -0,0 +1,40 @@
+/**
+ * ESP8266 HTTP Library
+ */
+
+//% color="#AA278D" weight=100 icon="\uf1eb"
+namespace esp8266http {
+
+    let tx = SerialPin.P15
+    let rx = SerialPin.P16
+    let baud = BaudRate.BaudRate115200
+
+    //% block="ESP8266 init TX %txPin RX %rxPin baud %baudRate"
+    export function init(txPin: SerialPin, rxPin: SerialPin, baudRate: BaudRate) {
+        tx = txPin
+        rx = rxPin
+        baud = baudRate
+        serial.redirect(tx, rx, baud)
+        basic.pause(2000)
+    }
+
+    //% block="ESP8266 send AT %cmd wait %ms ms"
+    export function sendAT(cmd: string, ms: number) {
+        serial.writeString(cmd + "\r\n")
+        basic.pause(ms)
+    }
+
+    //% block="ESP8266 HTTP GET host %host path %path"
+    export function httpGet(host: string, path: string) {
+
+        sendAT("AT+CIPSTART=\"TCP\",\"" + host + "\",80", 2000)
+
+        let req =
+            "GET " + path + " HTTP/1.1\r\n" +
+            "Host: " + host + "\r\n" +
+            "Connection: close\r\n\r\n"
+
+        sendAT("AT+CIPSEND=" + req.length, 1000)
+        sendAT(req, 3000)
+    }
+}
